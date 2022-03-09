1df7338882804f2fd96592cb8b87471a1c977958
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-03-09 18:40:46 +0500

    add footer

---
+++ movies-explorer/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__nav">
+        <p>&copy; 2022</p>
+        <ul className="footer__links">
+          <li>Яндекс.Практикум</li>
+          <li>GitHub</li>
+          <li>Facebook</li>
+        </ul>
+    </div>
+  </footer>
+  )
+}
+
+export default Footer;
