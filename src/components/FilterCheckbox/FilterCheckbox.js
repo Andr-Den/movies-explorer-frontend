d23bbc4dc4fbc8df5e6a1a1e4af0d064712eedc9
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-03-30 01:44:12 +0500

    fix default values on savedMovies page

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './FilterCheckbox.css'
 
-function FilterCheckbox({onClick}) {
+function FilterCheckbox({onClick, page}) {
   return (
     <div className="filter">
       <label className="filter__label">
-        <input type="checkbox" className="filter__checkbox filter__checkbox_visible" onClick={onClick} defaultChecked={JSON.parse(localStorage.getItem('checkbox'))}/>
+        <input type="checkbox" className="filter__checkbox filter__checkbox_visible" onClick={onClick} defaultChecked={page ? '' : JSON.parse(localStorage.getItem('checkbox'))}/>
         <span className="filter__checkbox_visible"></span>
       </label>
       <span>Короткометражки</span>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,18 +4,18 @@ import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
 import search from '../../images/search.svg'
 
-function SearchForm({onSubmit, onChange, errorName, isSearchValid, onClick}) {
+function SearchForm({onSubmit, onChange, errorName, isSearchValid, onClick, page}) {
 
   return (
     <div className="search-form">
       <div className="search-form__container">
         <img src={search} alt="поиск" className="search-form__icon"/>
         <form className="search-form__search" onSubmit={onSubmit}>
-          <input className="search-form__input" onChange={onChange} defaultValue={localStorage.getItem('input')}/>
+          <input className="search-form__input" onChange={onChange} defaultValue={page ? '' : localStorage.getItem('input')}/>
           {!isSearchValid ? <span className="search__input-error">{errorName}</span> : ''}
           <button type="submit" className="search-form__button"></button>
         </form>
-        <FilterCheckbox onClick={onClick}/>
+        <FilterCheckbox onClick={onClick} page={page}/>
       </div>
     </div>
   )
