080984a54e7f4185745bfcf3891f9557b332a217
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-03-29 23:02:21 +0500

    delete placeholder

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -11,7 +11,7 @@ function SearchForm({onSubmit, onChange, errorName, isSearchValid, onClick}) {
       <div className="search-form__container">
         <img src={search} alt="поиск" className="search-form__icon"/>
         <form className="search-form__search" onSubmit={onSubmit}>
-          <input placeholder="Фильм" className="search-form__input" onChange={onChange} defaultValue={localStorage.getItem('input')}/>
+          <input className="search-form__input" onChange={onChange} defaultValue={localStorage.getItem('input')}/>
           {!isSearchValid ? <span className="search__input-error">{errorName}</span> : ''}
           <button type="submit" className="search-form__button"></button>
         </form>
